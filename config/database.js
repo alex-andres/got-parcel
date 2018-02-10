e2555ef1b6171f4787eeebc551bfe1cae5cc48f0
Negin <negin.namiranian@gmail.com>
2018-02-09 16:57:01 -0800

    Changed the structure

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url': process.env.MONGODB_URI ||
-		'mongodb://uclax-gotparcel:CodeDragons2017@ds223738.mlab.com:23738/gotparcel'
+	'url': process.env.MONGODB_URI || 'mongodb://localhost/gotparcel'
 
 };
